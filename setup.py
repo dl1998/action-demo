99177bef5e1a8d6a20387d68e50f2f5a3714a4a7
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-09-04 21:38:35 +0200

    Update documentation pipeline

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ URL = 'https://github.com/dl1998/action-demo'
 EMAIL = 'dima.leschenko1998@gmail.com'
 AUTHOR = 'Dmytro Leshchenko'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 RELEASE = VERSION
 
 REQUIRED = [
