c54de5a474b5982d028fe560e2bddf01fc40daf4
Dmytro Leshchenko <dima.leschenko1998@gmail.com>
2023-09-04 21:06:45 +0200

    Update workflow

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ URL = 'https://github.com/dl1998/action-demo'
 EMAIL = 'dima.leschenko1998@gmail.com'
 AUTHOR = 'Dmytro Leshchenko'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 RELEASE = VERSION
 
 REQUIRED = [
